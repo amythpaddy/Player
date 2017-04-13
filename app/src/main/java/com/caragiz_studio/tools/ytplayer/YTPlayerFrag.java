8ee5304e73a5d351fa748861ec117728b19d169c
amythpaddy <amythpaddy21@gmail.com>
2017-04-13 20:19:22 +0530

    Build 1

---
+++ app/src/main/java/com/caragiz_studio/tools/ytplayer/YTPlayerFrag.java
@@ -0,0 +1,33 @@
+package com.caragiz_studio.tools.ytplayer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
+/**
+ * Created by caragiz on 11/4/17.
+ */
+
+public class YTPlayerFrag extends YouTubePlayerSupportFragment implements YouTubePlayer.OnInitializedListener{
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
+        super.onCreateView(layoutInflater, viewGroup, bundle);
+        View view = layoutInflater.inflate(R.layout.activity_main, null);
+        return view;
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+}
---
+++ app/src/main/java/com/caragiz_studio/tools/ytplayer/helper/Config.java
@@ -0,0 +1,14 @@
+package com.caragiz_studio.tools.ytplayer.helper;
+
+/**
+ * Created by caragiz on 28/3/17.
+ */
+
+public class Config {
+    // Google Console APIs developer key
+    // Replace this key with your's
+    public static final String YOUTUBE_API_KEY = "AIzaSyD6mWHXNb6idrFVqevUYeHgAyPo_JVaYnA";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}
